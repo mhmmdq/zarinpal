33bce1b10ebba142d4c5a352e6ae09babb4b9867
mhmmdq <mhmmdq@mhmmdq.ir>
2021-09-26 04:32:24 +0330

    update request.php from test files

--- test/request.php
+++ test/request.php
@@ -3,7 +3,14 @@
     include  '../vendor/autoload.php';
 
     use Mhmmdq\Zarinpal\Zarinpal;
+    
+    $merchant = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+    $amount = 200000;
+    $callback_url = 'https://zarinpal.mhmmdq.ir/verify.php';
+    $description = 'توضیحات پرداخت';
+    $metadata = ['name' => 'mhmmdq'];
 
-    $zarinpal = new Zarinpal('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' , 10000 , 'http://localhost/zarinpal-payment-class/verify.php' , 'یک پرداخت برای تست کلاس پرداخت با زرین پال' , ['name' => 'mhmmdq']);
+    $zarinpal = new Zarinpal($merchant , $amount , $callback_url , $description , $metadata);
 
+    $zarinpal->PayMentPortal();
     
